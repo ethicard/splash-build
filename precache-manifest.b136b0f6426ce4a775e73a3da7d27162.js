ceeb6e94e2ff056409c3ec870aaded7db387d672
theLAZYmd <theLAZYmd@gmail.com>
2020-10-13 13:46:07 +0100

    Updates

---
+++ precache-manifest.b136b0f6426ce4a775e73a3da7d27162.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669f0a7f1c036e506f8cc182e8eb304c",
+    "url": "/splash-build/index.html"
+  },
+  {
+    "revision": "6d7c85386d26857818ae",
+    "url": "/splash-build/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "847a37d0bca162996206",
+    "url": "/splash-build/static/css/main.9fbe1a1d.chunk.css"
+  },
+  {
+    "revision": "6d7c85386d26857818ae",
+    "url": "/splash-build/static/js/2.e41aa9bd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/splash-build/static/js/2.e41aa9bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847a37d0bca162996206",
+    "url": "/splash-build/static/js/main.beff4724.chunk.js"
+  },
+  {
+    "revision": "6a6a69522ae1ec8a10f8",
+    "url": "/splash-build/static/js/runtime-main.8784ca90.js"
+  }
+]);
